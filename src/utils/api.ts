734af09d2ec945ca9d3677eac29b7e8cc1822052
rvmahesangenix <mahesan.vanniaperumal@genixtec.com>
2025-03-06 10:25:31 +0530

    kev11 deployed version

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-let api_host = `http://ec2-54-174-67-117.compute-1.amazonaws.com/v1`; //"http://localhost:9380/v1"; //
+let api_host = `/v1`; //"http://localhost:9380/v1"; //http://ec2-44-212-5-246.compute-1.amazonaws.com
 
 export { api_host }; 
 
